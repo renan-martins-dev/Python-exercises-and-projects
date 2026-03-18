2ffd43fb6c3c93f4e962c314ef451fe7a0eecb88
Renan Martins <renanmartynzf@gmail.com>
2026-03-17 23:20:48 -0300

    First Python file

---
+++ Input-Output-and-Variables/exercise01greeting.py
@@ -0,0 +1,9 @@
+"""
+Exercise: 01 - Greeting 
+Topic: Input, Output and Variables
+Description:
+    Prints the entered user name.
+"""
+
+username = input("\nPlease, enter your name:\n")
+print(f"\nHello, {username}!")
